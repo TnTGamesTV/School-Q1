b8c04039cceaea869854e09b51878f8c1e27ad39
TnTGamesTV <finn.tegeler@web.de>
2017-10-05 20:33:13 +0200

    Updated RandomIntSupplier

--- Sortieren und Sortieren/src/com/github/tntgamestv/RandomIntSupplier.java
+++ Sortieren und Sortieren/src/com/github/tntgamestv/RandomIntSupplier.java
@@ -6,26 +6,26 @@ package com.github.tntgamestv;
 import java.util.function.IntSupplier;
 
 /**
- * @author TnTGamesTV
- * Project: Sortieren und Sortieren
- * Date: 26-09-2017
+ * @author TnTGamesTV Project: Sortieren und Sortieren Date: 26-09-2017
  */
 public class RandomIntSupplier implements IntSupplier {
 
-	private int start;
-	private int end;
-	
+	private int	start;
+	private int	end;
+
 	public RandomIntSupplier(int start, int end) {
-		this.start )= 
+		this.start = start;
+		this.end = end;
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.util.function.IntSupplier#getAsInt()
 	 */
 	@Override
 	public int getAsInt() {
-		// TODO Auto-generated method stub
-		return 0;
+		return (int) (Math.random() * end) + 1;
 	}
 
 }
